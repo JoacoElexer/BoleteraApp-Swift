4d087063f5d7ef4ec4676b2ca00eab4649b7b1e6
LIC-N1 <lic-n1@Macmini.local>
2026-04-24 22:14:25 -0600

    ediciond de usuario

---
+++ BoleteraApp/Views/Tabs/UsuarioConfig.swift
@@ -0,0 +1,18 @@
+//
+//  Usuario.swift
+//  BoleteraApp
+//
+//  Created by LIC-N1 on 24/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct UsuarioConfig: View {
+    var body: some View {
+        ZStack{
+            LinearGradient(colors: [Color.green, Color.blue], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
+            
+        }
+    }
+}
